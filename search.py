c48b20056402222f137d9671e50a79e24ba889fe
Nton118 <114150459+Nton118@users.noreply.github.com>
2023-06-30 22:15:20 +0300

    caching redis corrected

--- search.py
+++ search.py
@@ -12,35 +12,26 @@ cache = RedisLRU(client)
 def search_mongo(command, value):
     result = ""
     match command:
-            case "name":
-                name = str(value[0]).strip()
-                try:
-                    quotes = Quote.objects(
-                        author=Author.objects.get(fullname__startswith=name)
-                    )
-                except Exception as err:
-                    return err
-                for quote in quotes:
-                    result += (quote.quote) + "\n"
+        case "name":
+            name = value
+            try:
+                quotes = Quote.objects(
+                    author=Author.objects.get(fullname__startswith=name)
+                )
+            except Exception as err:
+                return err
+            for quote in quotes:
+                result += (quote.quote) + "\n"
 
-            case "tag":
-                tag = str(value[0]).strip()
-                try:
-                    quotes = Quote.objects(tags__startswith=tag)
-                except Exception as err:
-                    return err
-                for quote in quotes:
-                    result += (quote.quote) + "\n"
+        case "tag":
+            tag = value
+            try:
+                quotes = Quote.objects(tags__startswith=tag)
+            except Exception as err:
+                return err
+            for quote in quotes:
+                result += (quote.quote) + "\n"
 
-            case "tags":
-                tags_ = str(value[0]).strip().split(",")
-                tags = [tag.strip() for tag in tags_]
-                try:
-                    quotes = Quote.objects(tags__in=tags)
-                except Exception as err:
-                    return err
-                for quote in quotes:
-                    result += (quote.quote) + "\n"
     return result
 
 if __name__ == "__main__":
@@ -50,8 +41,21 @@ if __name__ == "__main__":
     while True:
         command, *value = input(">>>").split(":")
         
-        if command in ("name", "tag", "tags"):
-            print(search_mongo(command, value))
+        if command in ("name", "tag"):
+            val = str(value[0]).strip()
+            print(search_mongo(command, val))
+        
+        elif command == "tags":
+            result = ""
+            tags_ = str(value[0]).strip().split(",")
+            tags = [tag.strip() for tag in tags_]
+            try:
+                quotes = Quote.objects(tags__in=tags)
+            except Exception as err:
+                print(err)
+            for quote in quotes:
+                result += (quote.quote) + "\n"
+            print(result)
             
         elif command == "exit":    
             print("good bye!")
